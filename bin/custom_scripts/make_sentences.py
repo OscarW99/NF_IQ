d4134220ee202f460f59d4a30303ef9c189bfacf
Oscar Wright <oscar-wright@mail.com>
2022-07-19 17:21:57 -0400

    testing python argparse

--- bin/custom_scripts/make_sentences.py
+++ bin/custom_scripts/make_sentences.py
@@ -2,43 +2,42 @@
 import argparse
 import pandas as pd
 
-print('Hello World!')
 
-# # Create the parser
-# my_parser = argparse.ArgumentParser(
-#     description="Read a CSV file and create sentences with data")
+# Create the parser
+my_parser = argparse.ArgumentParser(
+    description="Read a CSV file and create sentences with data")
 
-# # Add the arguments
-# my_parser.add_argument('-d', '--data',
-#                        dest="input",
-#                        help="csv input file",
-#                        type=str, required=True)
+# Add the arguments
+my_parser.add_argument('-d', '--data',
+                       dest="input",
+                       help="csv input file",
+                       type=str, required=True)
 
-# # Execute the parse_args() method
-# args = my_parser.parse_args()
+# Execute the parse_args() method
+args = my_parser.parse_args()
 
-# input_path = args.input
-# # input_path = "/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/data/PRIV_GITHUB/csvData.csv"
+input_path = args.input
+# input_path = "/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/data/PRIV_GITHUB/high_IQ.csv"
 
-# ########################################
+########################################
 
-# # Read in csv file
+# Read in csv file
 
-# data = pd.read_csv(input_path)[1:5]
-
-# print('hello world')
-# print('input_path')
-
-
-# for index, row in data.iterrows():
-#     file = "/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/data/PRIV_GITHUB/" + \
-#         row['country'].replace(' ', '_') + '.txt'
-#     with open(file, 'w') as f:
-#         f.write("%s has a population of %s and an average IQ of %s.\n" %
-#                 (row['country'], row['pop2021'], row['finalIq']))
-#         f.close()
+data = pd.read_csv(input_path)[1:5]
+print(data)
+print('hello world')
+print('input_path')
 
 
-# # linux command
-# # touch test.txt | cat South_Korea.txt Japan.txt Hong_Kong.txt China.txt >> test.txt
-# ###
+for index, row in data.iterrows():
+    file = "/ahg/regevdata/projects/lungCancerBueno/Results/10x_nsclc_41421/data/PRIV_GITHUB/" + \
+        row['country'].replace(' ', '_') + '.txt'
+    with open(file, 'w') as f:
+        f.write("%s has a population of %s and an average IQ of %s.\n" %
+                (row['country'], row['pop2021'], row['finalIq']))
+        f.close()
+
+
+# linux command
+# touch test.txt | cat South_Korea.txt Japan.txt Hong_Kong.txt China.txt >> test.txt
+###
